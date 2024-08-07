f71a3c82fabdf7226fa21c933401c06a73ebf377
dreadedbird <legionelite6@gmail.com>
2024-08-07 17:21:34 +0700

    Add Basic Features to VoiceState * More info about voice state * Now it is possible to exclude user(s) from getting logged * Now it is logging mute status

--- cogs/voice_state.py
+++ cogs/voice_state.py
@@ -1,4 +1,3 @@
-import discord
 from discord.ext import commands
 import config
 
@@ -10,11 +9,28 @@ class VoiceState(commands.Cog):
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         channel = self.bot.get_channel(self.mod)
-        if before.channel is None:
-            if after.channel.id is config.create:
-                return
-            elif after.channel.id:
-                await channel.send(f"{member.mention}, joined {after.channel.mention}")
+        if member.id in config.excluded_user:
+            return
+        else:
+            if before.channel is None:
+                if after.channel.id == config.create:
+                    return
+                elif after.channel.id:
+                    await channel.send(f"{member.mention}, joined {after.channel.mention}")
+            if after.channel is None:
+                if before.channel.id == config.create:
+                    return
+                else:
+                    await channel.send(f"{member.mention}, left {before.channel.mention}")
+            if before.channel and after.channel and after.channel.id != config.create:
+                if before.channel.id == config.create:
+                    await channel.send(f"{member.mention} created a channel > {after.channel.mention}")
+                elif before.channel.id and after.channel.id and after.channel.id != before.channel.id:
+                    await channel.send(f"{member.mention} switched from {before.channel.mention} to {after.channel.mention}")
+                elif before.self_mute is False and after.self_mute is True:
+                    await channel.send(f"{member.mention} muted")
+                elif before.self_mute is True and after.self_mute is False:
+                    await channel.send(f"{member.mention} unmuted")
 
 async def setup(bot):
     await bot.add_cog(VoiceState(bot))
