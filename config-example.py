ccd0e2380a72a5e276c571530df4e00f0d702d3d
dreadedbird <legionelite6@gmail.com>
2024-08-08 18:31:40 +0700

    update config-example

--- config-example.py
+++ config-example.py
@@ -1,6 +1,7 @@
 token = 't0k3n' # add your bot token(str) here REQUIRED!
 guild = 12345 # add your guild id(int) here REQUIRED!
 mod_channel = 12345 # add your mod channel id(int) REQUIRED!
+invite = "https://discord.gg/GNHsATX7" # add your discord invite link(str) REQUIRED!
 create = 12345 # if you're using Voice Master put voice master channel id id(int) here if not then just leave it be
 excluded_user = [12345, 67890] # add excluded user(s) id(int) to this array(including voice master user id)... 
                                # if you want to exclude users from getting logged
