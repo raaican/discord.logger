75133ebd40841e9a50b66ce8bb1c7ae73ce872c6
dreadedbird <legionelite6@gmail.com>
2024-08-10 04:08:45 +0700

    Updated cogs/slash_commands.py * add new commands: - create => Create a voice channel and category(optional) - remove => Remove a voice channel

--- cogs/slash_commands.py
+++ cogs/slash_commands.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 from discord import app_commands, Interaction
 import config
@@ -22,9 +23,39 @@ class SlashCommands(commands.Cog):
         else:
             await interaction.response.send_message("User not found")
 
+    @app_commands.command(name="create", description="Create a voice channel")
+    async def create(self, interaction: Interaction, name: str, category: str = "Default"):
+        guild = interaction.guild
+        if interaction.user.id not in config.mods:
+            await interaction.response.send_message("no")
+            return
+        else:
+            if category:
+                category_name = discord.utils.get(guild.categories, name=category)
+                if not category_name:
+                    category_name = await guild.create_category(name=category)
+            await guild.create_voice_channel(name, category=category_name)
+            await interaction.response.send_message(f"{name} created")
+
+    @app_commands.command(name="remove", description="Remove a voice channel")
+    async def remove(self, interaction: Interaction, name: str):
+        if interaction.user.id not in config.mods:
+            await interaction.response.send_message("no")
+            return
+        else:
+            guild = interaction.guild
+            channel = discord.utils.get(guild.voice_channels, name=name)
+            if channel:
+                await channel.delete()
+                await interaction.response.send_message(f"{name} removed")
+            else:
+                await interaction.response.send_message("Channel not found")
+
     async def cog_unload(self):
         self.bot.tree.remove_command(self.invite.name)
         self.bot.tree.remove_command(self.profile.name)
+        self.bot.tree.remove_command(self.create.name)
+        self.bot.tree.remove_command(self.remove.name)
 
 
 async def setup(bot):
