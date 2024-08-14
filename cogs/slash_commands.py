cf6fd5e4a25e2d0eca3492f1e37bb1e5342ef289
dreadedbird <legionelite6@gmail.com>
2024-08-14 19:02:08 +0700

    Categorizing and code cleanup

--- cogs/slash_commands.py
+++ cogs/slash_commands.py
@@ -1,4 +1,3 @@
-import discord
 from discord.ext import commands
 from discord import app_commands, Interaction
 import config
@@ -11,34 +10,6 @@ class SlashCommands(commands.Cog):
     @app_commands.command(name="invite", description="Get invite link")
     async def invite(self, interaction: Interaction):
         await interaction.response.send_message(f"{config.invite}")
-
-    @app_commands.command(name="create", description="Create a voice channel")
-    async def create(self, interaction: Interaction, name: str, category: str = config.default_category, limit: int = 0):
-        guild = interaction.guild
-        if interaction.user.id not in config.mods:
-            await interaction.response.send_message("no")
-            return
-        else:
-            if category:
-                category_name = discord.utils.get(guild.categories, name=category)
-                if not category_name:
-                    category_name = await guild.create_category(name=category)
-            await guild.create_voice_channel(name, category=category_name, user_limit=limit)
-            await interaction.response.send_message(f"{name} created")
-
-    @app_commands.command(name="remove", description="Remove a voice channel")
-    async def remove(self, interaction: Interaction, name: str):
-        if interaction.user.id not in config.mods:
-            await interaction.response.send_message("no")
-            return
-        else:
-            guild = interaction.guild
-            channel = discord.utils.get(guild.voice_channels, name=name)
-            if channel:
-                await channel.delete()
-                await interaction.response.send_message(f"{name} removed")
-            else:
-                await interaction.response.send_message("Channel not found")
 
 
 async def setup(bot):
