9afc829ee4ac62db12439e0bb3997a974ce5d56b
dreadedbird <legionelite6@gmail.com>
2024-08-18 23:18:41 +0700

    clean: remove print

--- cogs/to_meme.py
+++ cogs/to_meme.py
@@ -11,9 +11,7 @@ class Meme(commands.Cog):
         if ctx.author.id not in config.mods:
             return
         if not ctx.message.reference:
-            print("no reference")
             return
-
         ref = await ctx.channel.fetch_message(ctx.message.reference.message_id)
         result = memes(ref.content)
         await ctx.send(result, reference=ctx.message.reference)
