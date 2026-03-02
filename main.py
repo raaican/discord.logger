5cf2406d9979e88ed3f6a43782a37e8e0e1125a1
raican <raaicn@gmail.com>
2026-03-02 22:17:17 +0700

    I should have just rewrite ts at this point

--- main.py
+++ main.py
@@ -13,10 +13,8 @@ class Main(commands.Bot):
 
     async def setup_hook(self):
         await self.load_extension("cogs.slash_commands")
-        await self.load_extension("cogs.reaction_assign")
         await self.load_extension("cogs.mod_slash_commands")
         await self.load_extension("cogs.osu_related")
-        await self.load_extension("cogs.to_meme")
         guild = discord.Object(id=config.guild)
         self.tree.copy_global_to(guild=guild)
         await self.tree.sync(guild=guild)
@@ -29,26 +27,20 @@ bot = Main()
 
 @tasks.loop(seconds=59)
 async def change_status():
-    global i
-    game = iter([
-        discord.Activity(name="Yorushika",
-                         type=discord.ActivityType.listening),
-        discord.Game(name="osu!"),
-        discord.Activity(name="Brazil",
-                         type=discord.ActivityType.competing),
-        discord.Activity(name="the Sky",
-                         type=discord.ActivityType.watching),
-        ])
+    activities = [
+        discord.Game(name="Listening to /invite"),
+        discord.Game(name="Listening to /osu_link"),
+        discord.Game(name="Listening to Server activity"),
+    ]
 
-    for i in range(random.randint(1, 4)):
-        i = next(game)
-    await bot.change_presence(activity=i)
+    activity = random.choice(activities)
+    await bot.change_presence(activity=activity)
 
 
 @bot.event
 async def on_ready():
     change_status.start()
-    print(f"Logged in as {bot.user} (ID: {bot.user.id})!")
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
     print("===================================================")
 
 bot.run(config.token)
